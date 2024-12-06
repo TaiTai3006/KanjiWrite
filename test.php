97ce9d564e66e88a887073d83e21bdd2fce9f223
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2024-12-06 17:53:38 +0700

    fix frint safari

--- test.php
+++ test.php
@@ -16,9 +16,42 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Kiểm tra kết nối
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
-}else{
-    echo "Kết nối thành công";
 }
+
+function getRealIPAddress(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //check ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //to check ip is pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$ip = getRealIPAddress();  
+echo $ip;
+
+
+// Chuẩn bị câu lệnh SQL
+// $sql = "INSERT INTO user (ip) VALUES (?)";
+
+// // Sử dụng prepared statement để bảo mật
+// $stmt = $conn->prepare($sql);
+// $stmt->bind_param("s", $ip);
+
+// // Thực thi câu lệnh và kiểm tra kết quả
+// if ($stmt->execute()) {
+//     echo "Thêm dữ liệu thành công!";
+// } else {
+//     echo "Lỗi: " . $stmt->error;
+// }
+
+// // Đóng kết nối
+// $stmt->close();
+// $conn->close();
 ?>
 
 </body>
